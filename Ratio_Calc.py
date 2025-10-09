f2409518f53b240686358f97ff0b25fb1f8f3209
Josh H <rhodges12@hotmail.com>
2025-10-09 14:25:13 -0400

    -finished? recipe obj create IO for saving/calling

--- Ratio_Calc.py
+++ Ratio_Calc.py
@@ -7,41 +7,65 @@ OZ2LB=16  #Ounces to 1lb
 G2KG2MG=1000  #1k g/kg 1k mg/g
 
 def saveRecipe(instring):
-    jDict=json.loads(instring)
+    jDict=loadRecipe(instring)
     #precondition is that json values match exactly, and 
-    inRecipe=Recipe(jDict['name'], jDict['description'], jDict['ingredients'], jDict['notes'],jDict['unit'])
+    inRecipe=makeRecipe(jDict)
+    return inRecipe.ratJason()
+
+def getRecipe(instring):
+    jDict=loadRecipe(instring)
+    inrecipe=makeRecipe(jDict)
+    return inrecipe.wtJson()
+
+'''If we are updating/iterating a recipe, I will return a pk of -1 this will signal 
+on the front end, that the key for this recipe needs to be new and the parent key needs to be 
+updated to the key of the recipe just iterated.'''
+def updateRecipe(instring):
+    jDict=loadRecipe(instring)
+    inrecipe= makeRecipe(jDict)
+    inrecipe.pk="-1"
+
+def makeRecipe(inDict):
+    return Recipe(inDict['name'], inDict['description'], inDict['ingredients'], inDict['notes'],inDict['unit'], inDict['totwt'], inDict['parentkey'])
+
 
 
 def loadRecipe(instring):
     return json.loads(instring)
 
 
-
+'''This next section is for the conversion logic if we are changin units. The ratios and weights will 
+be easily updated by passing in updated weight variables. because the re'''
 class RatIngredient:
     def __init__(self, name, ratio):
         self.what=name
         self.ratio=ratio
 
+
 class WtIngredient:
     def __init__(self, name, wt):
+        self.what=name
+        self.wt=wt
         
 
 class Recipe:
     
-    def __init__(self, name, description, ingredients, notes, units):
+    def __init__(self, name, description, ingredients, notes, units, totwt, parentKey):
         self.name=name
         self.description=description
-        self.totwt = self.getTotWt(ingredients)
+        self.totwt = totwt
         self.ratingredients = self.getRatIngredients(ingredients)
         self.wtingredients = self.getWtIngredients(ingredients)
         self.notes=notes
         self.unit=units
+        self.pk=parentKey
         
-    def getTotWt(inglst):
-        total=0
-        for i in inglst:
-            total+=float(i['weight'])
-        return total        
+    # def getTotWt(self, inglst):
+    #     if self.totwt == null:
+    #         total=0
+    #         for i in inglst:
+    #             total+=float(i['weight'])
+    #         return total        
 
     def getRat(self, wt):
         return round(wt /self.totwt, 4)
@@ -82,10 +106,24 @@ class Recipe:
         return ingredients  
 
     def getWt(self, ratio):
-        return round(ratio*self.totwt, 2)      
+        return round(ratio * self.totwt, 2) 
 
+    def toDict(self, type):
+        if type=='rat':
+            ingType = self.ratingredients
+        elif type == 'wt':
+            ingType = self.wtingredients
+        else:
+            print('passed wrong variable')
+        return {'name':self.name, 'description':self.description, 'ingredients':ingType, 'notes':self.notes, 'units':self.units, 'totwt':self.totwt, 'parentKey':self.pk}    
+
+    def ratJson(self):
+        ratdict=self.toDict('rat')
+        return json.dump(ratdict)
     
-
+    def wtJson(self):
+        wtDict=self.toDict('wt')
+        return json.dump(wtDict)
 
 # if __name__ == "__main__":
 #     getjsonstring()
